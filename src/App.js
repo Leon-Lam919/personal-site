f44ab3d60932485dec6e930f6f1b2f81fb9beeb8
Leon Lam <leonlam34@gmail.com>
2025-04-08 13:46:46 -0500

    adjusted title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" style={{
+      display: "flex", justifyContent: "center", alignItems: "center", height: "100vh",
+    }} >
+      <Stack direction="row" spacing={2} alignItems="center">
+        <h1 style={{ color: "#32364F" }}> Leon </h1>
+        <Avatar alt="Leon profile pic" sx={{ width: 300, height: 300 }} src="/Me.jpg" />
+        <h1 style={{ color: "#32364F" }}> Lam </h1>
+      </Stack>
+    </div >
   );
 }
 
