4d0fdc38df77589e4752d627b02decfb10014463
Leon Lam <leonlam34@gmail.com>
2025-04-17 16:03:01 -0500

    worked on project timeline, CSS is hard

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Stack from '@mui/material/Stack';
 function App() {
   return (
     <div className="App" style={{
-      display: "flex", justifyContent: "center", alignItems: "center", height: "100vh",
+      display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "100vh", gap: '2rem'
     }} >
       <Stack direction="row" spacing={2} alignItems="center">
         <h1 style={{ color: "#32364F" }}> Leon </h1>
@@ -14,12 +14,29 @@ function App() {
         <h1 style={{ color: "#32364F" }}> Lam </h1>
       </Stack>
 
-      <section className="timeline">
-        <div className="timeline left"> Project 1 </div>
-        <div className="timeline right"> Project 2 </div>
-        <div className="timeline left"> Project 3</div>
-        <div className="timeline right"> Project 4</div>
-        <div className="timeline left">Project 5 </div>
+      <section className="project">
+        <div className="project-item project-left">
+          <h3 className='project-title'>SmileWorks</h3>
+          <p className='project-title'>
+            Created a full stack application working mainly in the backend. Functionality included a working appointment system, inventory management, etc.
+          </p>
+        </div>
+        <div className="project-item project-right">
+          <h3 className='project-title'>Project 2 </h3>
+          <p className='project-title'>Project description right</p>
+        </div>
+        <div className="project-item project-left">
+          <h3 className='project-title'>Project 3 </h3>
+          <p className='project-title'>Project description left</p>
+        </div>
+        <div className="project-item project-right">
+          <h3 className='project-title'>Project 2 </h3>
+          <p className='project-title'>Project description right</p>
+        </div>
+        <div className="project-item project-left">
+          <h3 className='project-title'>Project 5 </h3>
+          <p className='project-title'>Project description left</p>
+        </div>
       </section>
     </div >
   );
