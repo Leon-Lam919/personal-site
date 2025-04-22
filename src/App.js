27800b969289e781c21e6f2ea4e2a6b2ff82bd0e
Leon Lam <leonlam34@gmail.com>
2025-04-22 11:48:02 -0500

    4/21, worked on background color and formatting timeline description points

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Stack from '@mui/material/Stack';
 
 function App() {
   return (
-    <div className="App" style={{
-      display: "flex", flexDirection: "column", alignItems: "center", height: "100vh", gap: '2rem'
-    }} >
+    <div className="App">
       <Stack direction="row" spacing={2} alignItems="center">
         <h1 style={{ color: "#32364F" }}> Leon </h1>
         <Avatar alt="Leon profile pic" sx={{ width: 300, height: 300 }} src="/Me.jpg" />
@@ -20,7 +18,7 @@ function App() {
           <h5 className='project-title'>Dental clinic Management system</h5>
           <p>A scalable, multi-tenant web appplication designed for dental clinics, featuring comprehensive tools for patient management, appointment scheduling, insurance verification, and inventory control. </p>
           <ul>
-            <li><b>Backend: </b>Developed using <b>Express</b> and <b>MongoDB</b>, with a focus on designing efficient scehams and building secure, <b>JWT-authenticated</b> RESTful APIs</li>
+            <li><b>Backend: </b>Developed using <b>Express</b> and <b>MongoDB</b>, with a focus on designing efficient schemas and building secure, <b>JWT-authenticated</b> RESTful APIs</li>
             <li><b>Frontend: </b> Built with <b>Next.js, TypeScript,</b>and <b>Tailwind CSS</b> for a responsive, modern user experience.</li>
             <li><b>Testing: </b> Ensured API reliability through testing in <b>Postman</b>.</li>
             <li><b>Collaboration: </b> Worked closely with a team and two team leads with decades of experience in industry to maintain clean archirecture and adhere to best practices.</li>
